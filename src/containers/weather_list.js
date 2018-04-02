e7cac5e90d9796784430e1c002370975e551699a
feng <liufeng_bupt@163.com>
2018-04-03 01:23:24 +0800

    google-map

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map'
+
 class WeatherList extends Component {
   renderWeather(cityData) {
     const bool_citydata = typeof cityData === 'undefined';
@@ -8,12 +10,12 @@ class WeatherList extends Component {
     const temps = bool_citydata ? null : cityData.list.map(weather => weather.main.temp);
     const pressures = bool_citydata ? null : cityData.list.map(weather => weather.main.pressure);
     const humidities = bool_citydata ? null : cityData.list.map(weather => weather.main.humidity);
-
+    const { lon, lat } = bool_citydata ? null : cityData.city.coord;
 
     if (!name) return ;
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td><Chart data={temps} color="orange" units="K"></Chart></td>
         <td><Chart data={pressures} color="green" units="hPa"></Chart></td>
         <td><Chart data={humidities} color="red" units="%"></Chart></td>
